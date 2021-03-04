c8112903d74113a516e83184bba9216cb5417a4f
LittleB30 <50643241+LittleB30@users.noreply.github.com>
2021-03-03 19:42:28 -0500

    printFF ready to test

--- printFF.java
+++ printFF.java
@@ -1,5 +1,16 @@
+/**
+ * Driver class to run the CFGrammer.
+ * @author Alex Smith (alsmi14)
+ */
 public class printFF {
     public static void main(String[] args) {
-        
+        // if (args.length < 1) {
+        //     System.out.println("Usage: printFF fileName);
+        //     return;
+        // }
+        // String file = args[0];
+        String file = "G1.txt";
+        CFGrammar grammar = new CFGrammar(file);
+        grammar.print();
     }
 }
